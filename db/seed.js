a4e4b82c37445bb0f6699a21a2be8f5a2828933b
Matthew Short <short.matthew.f@gmail.com>
2020-04-25 10:53:44 -0400

    end of day 1

--- db/seed.js
+++ db/seed.js
@@ -42,11 +42,10 @@ async function createInitialUsers() {
   try {
     console.log("Starting to create users...");
 
-    const albert = await createUser({ username: 'albert', password: 'bertie99' });
-    const albertTwo = await createUser({ username: 'albert', password: 'imposter_albert' });
-   
-    console.log(albert, albertTwo);
-
+    await createUser({ username: 'albert', password: 'bertie99' });
+    await createUser({ username: 'sandra', password: '2sandy4me' });
+    await createUser({ username: 'glamgal', password: 'soglam' });
+    
     console.log("Finished creating users!");
   } catch (error) {
     console.error("Error creating users!");
